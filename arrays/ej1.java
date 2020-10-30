6672df7ff01445d7ab91a15d8d1595402793a334
José Manuel <jbermudo@iesmiravent.es>
2020-10-30 17:09:42 +0100

    Ejercicio 1

--- arrays/ej1.java
+++ arrays/ej1.java
@@ -1,9 +1,33 @@
 package arrays;
 
+/**
+ * Crea un programa que sume todas las posiciones pares de un array de enteros.
+ * @author Jose Manuel Bermudo
+ *
+ */
+
 public class ej1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Supondremos un array ya creado:
+		int[] arrayEnteros = {1,2,3,4,5,6,7,8,9,10};
+		
+		//Esta variable llevará la suma parcial
+		int suma = 0;
+		
+		for (int i = 0; i < arrayEnteros.length; i=i+2) {
+			suma += arrayEnteros[i];
+		}
+		
+		// Otra forma de hacerlo
+//		for (int i = 0; i < arrayEnteros.length; i++) {
+//			if (i % 2 == 0) {
+//				suma += arrayEnteros[i];
+//			}
+//		}
+		
+		System.out.println("La suma de las posiciones pares del array es " + suma);
 
 	}
 
