eb8412bcee28e8d59a44e25ab78fcd2dde9303a6
José Manuel <jbermudo@iesmiravent.es>
2020-11-06 10:11:10 +0100

    Ejercicio 13

--- arrays/Ej13.java
+++ arrays/Ej13.java
@@ -23,21 +23,40 @@ public class Ej13 {
 		desapilar();
 		apilar(11);
 		
+		for (int i = 0; i < 11; i++) {
+			desapilar();
+		}
+		
 	}
 	
 	public static void apilar(int numero) {
+		
+		if (tamPila < pila.length) {
+			pila[tamPila++] = numero;
+			System.out.println("Apilo el número " + numero);
+		}
+		else {
+			System.out.println("La pila está llena. No puedo apilar el elemento");
+		}
 		
 		imprimeArray(pila);
 	}
 	
 	public static void desapilar() {
-		
 		System.out.println("Desapilar");
+		
+		if (tamPila > 0) {
+			tamPila--;
+		}
+		else {
+			System.out.println("No hay elementos en la pila");
+		}
+		
 		imprimeArray(pila);
 	}
 	
 	public static void imprimeArray(int[] array) {
-		for (int i = 0; i < array.length; i++) {
+		for (int i = 0; i < tamPila; i++) {
 			if (i > 0) {
 				System.out.print(", ");
 			}
