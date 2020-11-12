b5a1492d4195f57a24b657f5b2982e84de77c221
José Manuel <jbermudo@iesmiravent.es>
2020-11-12 10:14:25 +0100

    Ejercicio 15

---
+++ arrays/Ej15.java
@@ -0,0 +1,21 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Ej15 {
+
+	public static void main(String[] args) {
+		int[] eratostenes;
+		int tamArray;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Introduce el número máximo para saber los primos: ");
+		tamArray = Integer.parseInt(sc.nextLine());
+		
+		eratostenes = new int[tamArray];
+		
+		// 
+	}
+
+}
