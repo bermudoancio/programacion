005721fdc25d01f2421fca60b83d6fac6d84c444
José Manuel <jbermudo@iesmiravent.es>
2020-11-16 13:50:22 +0100

    Ejercicio 4

---
+++ boletin_04_matrices/Ej4.java
@@ -0,0 +1,23 @@
+package boletin_04_matrices;
+
+public class Ej4 {
+
+	public static void main(String[] args) {
+		int[][] matriz = {
+				{1,3,4,5},
+				{45,22,1,90},
+				{123,8,97,62}
+		};
+		
+		muestraPorFilasInversa(matriz);
+	}
+	
+	public static void muestraPorFilasInversa(int[][] matriz) {
+		for (int i = matriz.length - 1; i >= 0; i--) {
+			for (int j = matriz[i].length - 1; j >=0; j--) {
+				System.out.printf("%d ", matriz[i][j]);
+			}
+		}
+	}
+
+}
