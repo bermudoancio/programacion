e72544fa94e2be74be6195da286e2194ed439d76
José Manuel <jbermudo@iesmiravent.es>
2021-01-15 09:41:41 +0100

    Ejercicio nuevo

--- boletin_06b_objetos/MiEntradaSalida.java
+++ boletin_06b_objetos/MiEntradaSalida.java
@@ -85,4 +85,31 @@ public class MiEntradaSalida {
 		return integer;
 	}
 	
+	public static char solicitarCaracter(String mensaje) {
+		char c = '0';
+		
+		// Variable que almacenará un booleano que indicará si se le debe volver a pedir el dato al usuario.
+		boolean flag = true;
+		
+		while(flag) {
+			// Pedimos el entero por pantalla.
+			System.out.println(mensaje);
+			// Comprobamos si el usuario está introduciendo algo correcto usando la excepción del método parseInt.
+			try {
+				c = sc.nextLine().charAt(0);
+				// Si llegamos hasta aquí, es porque el usuario ha introducido un dato correcto y no se ha lanzado ninguna excepción.
+				flag = false;
+			}
+			// Si se lanza la excepción, informamos al usuario de su error.
+			catch(IndexOutOfBoundsException e) {
+				System.out.println("Ha introducido un dato incorrecto.");
+			}
+			
+		}
+		
+		return c;
+	}
+	
+	
+	
 }
